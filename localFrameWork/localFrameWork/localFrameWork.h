0974fafab0bc35f71c4ad13051b6a83285e19e0f
Umer Saleem <umer.saleem@wizlinx.com>
2019-01-07 12:49:38 +0500

    Finished our local framework

---
+++ localFrameWork/localFrameWork/localFrameWork.h
@@ -0,0 +1,19 @@
+//
+//  localFrameWork.h
+//  localFrameWork
+//
+//  Created by Shaukat Mahmood Ahmad on 07/01/2019.
+//  Copyright © 2019 Shaukat Mahmood Ahmad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for localFrameWork.
+FOUNDATION_EXPORT double localFrameWorkVersionNumber;
+
+//! Project version string for localFrameWork.
+FOUNDATION_EXPORT const unsigned char localFrameWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <localFrameWork/PublicHeader.h>
+
+
---
+++ localFrameWork/localFrameWork/service.swift
@@ -0,0 +1,19 @@
+//
+//  service.swift
+//  localFrameWork
+//
+//  Created by Shaukat Mahmood Ahmad on 07/01/2019.
+//  Copyright © 2019 Shaukat Mahmood Ahmad. All rights reserved.
+//
+
+import Foundation
+
+class Service {
+    
+    private init () {}
+    
+    public static func doSomeThing() -> String{
+    
+        return "this time you should create the custome Library"
+    }
+}
